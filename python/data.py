dfdcf7f4eb9bf9eae02bb868da9409a8b5d98aaf
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-04-07 16:56:25 +0530

    Dummy data has been added for testing

---
+++ python/data.py
@@ -0,0 +1,73 @@
+sample_posts = [
+    {
+        "user": "user1",
+        "post": "Hey how are you doing buddy, the weather is amazing today!",
+    },
+    {
+        "user": "user2",
+        "post": "I hate getting stuck in traffic every morning, it just ruins my mood",
+    },
+    {
+        "user": "user3",
+        "post": "Just finished my workout and feeling great, consistency really pays off",
+    },
+    {
+        "user": "user4",
+        "post": "This kind of toxic behavior is really not acceptable in any environment",
+    },
+    {"user": "user5", "post": "Enjoying a peaceful evening with coffee and some music"},
+    {
+        "user": "user1",
+        "post": "I love watching movies on weekends, you should try it from https://netflix.com",
+    },
+    {"user": "user2", "post": "Sometimes people act so baaaaaad for no reason at all"},
+    {
+        "user": "user3",
+        "post": "Planning a short trip this weekend, found some good places on https://tripadvisor.com",
+    },
+    {
+        "user": "user4",
+        "post": "That was such a tox*****ic comment, totally unnecessary and rude",
+    },
+    {
+        "user": "user5",
+        "post": "Reading a new novel these days, it’s actually quite interesting",
+    },
+    {
+        "user": "user1",
+        "post": "Feeling productive today, completed most of my pending work",
+    },
+    {
+        "user": "user2",
+        "post": "I had a really bad day today, hoping tomorrow gets better",
+    },
+    {
+        "user": "user3",
+        "post": "Good vibes only, trying to stay positive no matter what",
+    },
+    {
+        "user": "user4",
+        "post": "People should stop spreading hate online, it affects everyone",
+    },
+    {
+        "user": "user5",
+        "post": "Morning walks are refreshing, you should try starting your day like this",
+    },
+    {"user": "user1", "post": "Learning something new every day keeps things exciting"},
+    {
+        "user": "user2",
+        "post": "This behavior is just toxic and shouldn't be encouraged",
+    },
+    {
+        "user": "user3",
+        "post": "Listening to music and relaxing after a long day, check out https://spotify.com",
+    },
+    {
+        "user": "user4",
+        "post": "I absolutely hate how this situation turned out, really frustrating",
+    },
+    {
+        "user": "user5",
+        "post": "Tried a new recipe today and it turned out delicious, found it on https://food.com",
+    },
+]
